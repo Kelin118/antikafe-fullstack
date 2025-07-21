478f8f15451dacbaf95962f1f5e51a292da2cca8
Kelin118 <kivimynsky@gmail.com>
2025-07-21 13:01:22 +0500

    Добавлена логика удаления гостей и фильтр по датам, улучшена схема guests

--- antikafe-backend/models/Guest.js
+++ antikafe-backend/models/Guest.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const guestSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true }, // 👈
-  createdAt: { type: Date, default: Date.now }
-});
+  companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
+}, { timestamps: true });
 
 module.exports = mongoose.model('Guest', guestSchema);
